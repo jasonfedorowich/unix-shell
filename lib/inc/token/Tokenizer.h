6f4a708176032d909b78fbf18d83951746732a0a
jasonfedorowich <42303804+jasonfedorowich@users.noreply.github.com>
2026-02-22 15:49:01 -0500

    add tokenizer for input

--- lib/inc/token/Tokenizer.h
+++ lib/inc/token/Tokenizer.h
@@ -10,6 +10,37 @@
 
 #include "Token.h"
 
+struct Tokenizer {
+private:
+    void parse();
+    void push();
+    void parseBackSlash();
+    void parseSingleQuote();
+    void parseDoubleQuote();
+    void flush();
+    int index;
+    std::string input;
+    std::vector<std::string> tokens;
+    std::string buffer;
+public:
+
+
+    Tokenizer(const std::string& input) {
+        this->input = input;
+        this->index = 0;
+    }
+    char peek();
+    char next();
+    bool match(char expected);
+    bool isOutOfBounds();
+    void advance();
+
+    std::vector<std::string> tokenize();
+};
+
+
+
+
 //todo might be easier to handle tokens rather than strings down the road
 std::vector<std::string> tokenize(const std::string& text, char delim);
 std::vector<std::string> tokenize(const std::string& text);
--- lib/src/token/Tokenizer.cpp
+++ lib/src/token/Tokenizer.cpp
@@ -4,6 +4,98 @@
 
 #include "../../inc/token/Tokenizer.h"
 
+#include <ostream>
+
+void Tokenizer::parse() {
+   while (!isOutOfBounds()) {
+       switch (peek()) {
+           case '\'':
+               parseSingleQuote();
+               break;
+           case '\"':
+               parseDoubleQuote();
+               break;
+           case '\\':
+               parseBackSlash();
+               break;
+           case ' ':
+               flush();
+               break;
+           default:
+               push();
+               break;
+       }
+   }
+    flush();
+}
+
+void Tokenizer::push() {
+    buffer.push_back(next());
+}
+
+void Tokenizer::parseBackSlash() {
+    next();
+    if (!isOutOfBounds()) push();
+}
+
+void Tokenizer::parseSingleQuote() {
+    advance();
+    std::string current;
+    while (peek() != '\'') {
+        current.push_back(next());
+    }
+    advance();
+    buffer += current;
+}
+
+void Tokenizer::parseDoubleQuote() {
+    advance();
+    std::string current;
+    while (peek() != '"') {
+        current.push_back(next());
+    }
+    advance();
+    buffer += current;
+}
+
+void Tokenizer::flush() {
+    if (buffer.empty()) {
+        advance();
+        return;
+    }
+    tokens.push_back(buffer);
+    buffer.clear();
+    advance();
+}
+
+char Tokenizer::peek() {
+    if (!isOutOfBounds()) return input[index];
+    return '\0';
+}
+
+char Tokenizer::next() {
+    if (!isOutOfBounds()) return input[index++];
+    return '\0';
+}
+
+bool Tokenizer::match(char expected) {
+    if (!isOutOfBounds()) return input[index] == expected;
+    return false;
+}
+
+bool Tokenizer::isOutOfBounds() {
+    return this->index >= this->input.size();
+}
+
+void Tokenizer::advance() {
+    index++;
+}
+
+std::vector<std::string> Tokenizer::tokenize() {
+    parse();
+    return tokens;
+}
+
 std::vector<std::string> tokenize(const std::string &text, char delim) {
     int i = 0;
     std::vector<std::string> tokens;
@@ -21,47 +113,10 @@ std::vector<std::string> tokenize(const std::string &text, char delim) {
     return tokens;
 }
 
-static std::string getSingleQuotedString(const std::string &text, int& i) {
-    i++;
-    std::string str;
-    while (i < text.size() && text[i] != '\'') {
-        str.push_back(text[i]);
-        i++;
-    }
-    return str;
-}
-
-static std::string getDoubleQuotedString(const std::string &text, int& i) {
-    i++;
-    std::string str;
-    while (i < text.size() && text[i] != '\"') {
-        str.push_back(text[i]);
-        i++;
-    }
-    return str;
-}
 
 std::vector<std::string> tokenize(const std::string &text) {
-    int i = 0;
-    std::vector<std::string> tokens;
-    std::string token;
-    while (i < text.size()) {
-        if (text[i] == ' ') {
-            if (!token.empty()) {
-                tokens.push_back(token);
-                token.clear();
-            }
-        }else if (text[i] == '\'') {
-            token += getSingleQuotedString(text, i);
-        }else if (text[i] == '\"') {
-            token += getDoubleQuotedString(text, i);
-        }
-        else {
-            token.push_back(text[i]);
-        }
-        i++;
-    }
-    tokens.push_back(token);
+    Tokenizer tokenizer(text);
+    std::vector<std::string> tokens = tokenizer.tokenize();
     return tokens;
 }
 
